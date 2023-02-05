7e39525ced52c475b01f28160362849f8c7a4d12
neo <13758242402@163.com>
2023-02-05 14:52:39 +0800

    按erp重构

--- src/main/java/dml/common/id/repo/CommonIdGeneratorRepository.java
+++
@@ -1,9 +0,0 @@
-package dml.common.id.repo;
-
-import dml.common.id.aggr.IdGenerator;
-import dml.repository.TakeFromSingletonRepository;
-
-public interface CommonIdGeneratorRepository<E extends IdGenerator<?>> extends
-		TakeFromSingletonRepository<E> {
-
-}
