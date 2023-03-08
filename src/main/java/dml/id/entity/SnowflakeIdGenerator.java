54a241c0db238cc6801c07b9c621e6a2b80e0cbc
neo <13758242402@163.com>
2023-03-09 00:03:04 +0800

    添加 SnowflakeIdGenerator

---
+++ src/main/java/dml/id/entity/SnowflakeIdGenerator.java
@@ -0,0 +1,101 @@
+package dml.id.entity;
+
+import java.net.NetworkInterface;
+import java.security.SecureRandom;
+import java.time.Instant;
+import java.util.Enumeration;
+
+public class SnowflakeIdGenerator implements IdGenerator<Long> {
+
+    private static final int UNUSED_BITS = 1; // Sign bit, Unused (always set to 0)
+    private static final int EPOCH_BITS = 41;
+    private static final int NODE_ID_BITS = 10;
+    private static final int SEQUENCE_BITS = 12;
+
+    private static final long maxNodeId = (1L << NODE_ID_BITS) - 1;
+    private static final long maxSequence = (1L << SEQUENCE_BITS) - 1;
+
+    // Epoch (January 1, 2015 Midnight UTC = 2015-01-01T00:00:00Z)
+    private static final long EPOCH = 1420070400000L;
+
+    private final long nodeId;
+
+    private volatile long lastTimestamp = -1L;
+    private volatile long sequence = 0L;
+
+    // Create SnowflakeIdGenerator with a nodeId
+    public SnowflakeIdGenerator(long nodeId) {
+        if (nodeId < 0 || nodeId > maxNodeId) {
+            throw new IllegalArgumentException(String.format("NodeId must be between %d and %d", 0, maxNodeId));
+        }
+        this.nodeId = nodeId;
+    }
+
+    // Let SnowflakeIdGenerator generate a nodeId
+    public SnowflakeIdGenerator() {
+        this.nodeId = createNodeId();
+    }
+
+    private long createNodeId() {
+        long nodeId;
+        try {
+            StringBuilder sb = new StringBuilder();
+            Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+            while (networkInterfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = networkInterfaces.nextElement();
+                byte[] mac = networkInterface.getHardwareAddress();
+                if (mac != null) {
+                    for (byte macPort : mac) {
+                        sb.append(String.format("%02X", macPort));
+                    }
+                }
+            }
+            nodeId = sb.toString().hashCode();
+        } catch (Exception ex) {
+            nodeId = (new SecureRandom().nextInt());
+        }
+        nodeId = nodeId & maxNodeId;
+        return nodeId;
+    }
+
+    @Override
+    public Long generateId() {
+        long currentTimestamp = timestamp();
+
+        if (currentTimestamp < lastTimestamp) {
+            throw new IllegalStateException("Invalid System Clock!");
+        }
+
+        if (currentTimestamp == lastTimestamp) {
+            sequence = (sequence + 1) & maxSequence;
+            if (sequence == 0) {
+                // Sequence Exhausted, wait till next millisecond.
+                currentTimestamp = waitNextMillis(currentTimestamp);
+            }
+        } else {
+            // reset sequence to start with zero for the next millisecond
+            sequence = 0;
+        }
+
+        lastTimestamp = currentTimestamp;
+
+        long id = currentTimestamp << (NODE_ID_BITS + SEQUENCE_BITS)
+                | (nodeId << SEQUENCE_BITS)
+                | sequence;
+
+        return id;
+    }
+
+    // Get current timestamp in milliseconds, adjust for the epoch.
+    private long timestamp() {
+        return Instant.now().toEpochMilli() - EPOCH;
+    }
+
+    // Block and wait till next millisecond
+    private long waitNextMillis(long currentTimestamp) {
+        while (currentTimestamp == lastTimestamp) {
+            currentTimestamp = timestamp();
+        }
+        return currentTimestamp;
+    }
+}
